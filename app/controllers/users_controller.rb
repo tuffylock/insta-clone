b198d8e0180902c3b0a6a65098fa83911ef0abd9
tuffylock <tuffylock@gmail.com>
2019-10-22 11:59:03 -0700

    photo index placeholder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # temp
-      redirect_to "https://www.google.com"
+      redirect_back_or_to(:photos, notice: 'Welcome!')
     else
       flash.now[:alert] = @user.errors.full_messages
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
-  get 'create_account', to: 'users#new'
+  get 'create_account', to: 'users#new', as: :create_account
+
+  resources :photos, only: [:index]
 end
